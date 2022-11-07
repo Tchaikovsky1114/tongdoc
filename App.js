740c5688bfd98021f77824990847e9f5b975157f
MinYoungKIM <leminyoung122@gmail.com>
2022-11-07 16:04:07 +0900

    221107 작업내용 testKim Page 제작(디버깅용)

--- App.js
+++ App.js
@@ -6,22 +6,17 @@ import OnBoarding from "./components/onboarding/OnBoarding";
 import SignupPage from "./screens/SignupPage";
 import SigninPage from "./screens/SigninPage";
 import HomeScreen from "./screens/HomeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { enableScreens } from "react-native-screens";
+import Splash from "./components/Splash";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-
-import OnBoarding from './components/onboarding/OnBoarding';
-import SignupPage from './screens/SignupPage';
-import HomeScreen from './screens/HomeScreen';
-import { createNativeStackNavigator, } from '@react-navigation/native-stack';
-import {enableScreens} from 'react-native-screens'
-import Splash from './components/Splash';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen'
-
-import 'react-native-gesture-handler'
-import CertificationScreen from './screens/CertificationScreen';
-import CertificationResult from './components/certification/CertificationResult';
-import CertificationInProgress from './components/certification/CertificationInProgress';
-
+import "react-native-gesture-handler";
+import CertificationScreen from "./screens/CertificationScreen";
+import CertificationResult from "./components/certification/CertificationResult";
+import CertificationInProgress from "./components/certification/CertificationInProgress";
+import TestKim from "./components/testmin/TestMin";
 
 enableScreens();
 const Stack = createNativeStackNavigator();
@@ -58,19 +53,37 @@ export default function App() {
   if (!appIsReady) return <View></View>;
 
   return (
-
-    <View style={{flex: 1}} onLayout={onLayoutRootView} >
-      <NavigationContainer >
-        <Stack.Navigator screenOptions={{animation:"slide_from_right"}} >
-          <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}  />
-          <Stack.Screen name="OnBoarding" component={OnBoarding} options={{headerShown:false}} /> 
-          <Stack.Screen name="Home" component={HomeScreen}  />
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ animation: "slide_from_right" }}>
+          <Stack.Screen
+            name="Splash"
+            component={Splash}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="OnBoarding"
+            component={OnBoarding}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Signup" component={SignupPage} />
-          <Stack.Screen name="Signup/Certification" component={CertificationScreen} />
-          <Stack.Screen name="Signup/CertificationResult" component={CertificationResult} />
-          <Stack.Screen name="Signup/CertificationInProgress" component={CertificationInProgress} />
+          <Stack.Screen
+            name="Signup/Certification"
+            component={CertificationScreen}
+          />
+          <Stack.Screen
+            name="Signup/CertificationResult"
+            component={CertificationResult}
+          />
+          <Stack.Screen
+            name="Signup/CertificationInProgress"
+            component={CertificationInProgress}
+          />
           <Stack.Screen name="Signin" component={SigninPage} />
-
+          {/* 김민영 테스트용 시작 */}
+          <Stack.Screen name="TestKim" component={TestKim} />
+          {/* 김민영 테스트용 끝 */}
         </Stack.Navigator>
       </NavigationContainer>
     </View>
--- screens/SigninPage.jsx
+++ screens/SigninPage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import Signin from "../components/signin/Signin";
 
 export default function SigninPage() {
   return <Signin />;
 }
-const styles = StyleSheet.create({});
