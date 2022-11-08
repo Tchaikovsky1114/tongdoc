6a19b8359d2763365f2f986475b27b48d5482139
Tchaikovsky1114 <forwarm5891@gmail.com>
2022-11-08 15:22:05 +0900

    fix: input validation 수정

--- components/common/SignupInput.jsx
+++ components/common/SignupInput.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, TextInput } from 'react-native'
 import React,{useRef,useState,useEffect,forwardRef} from 'react'
+import { IgnorePattern } from 'react-native/Libraries/LogBox/LogBox'
 
 const SignupInput = forwardRef(({type,style,value,clearButtonMode,autoCapitalize,placeholder,keyboardType,onChange,errorText,clearTextOnFocus,secureTextEntry,maxLength},ref) => {
 
-  const [isError,setIsError] = useState(false)
-  const [startValidation,setStartValidation] = useState(false)
+  const [isError,setIsError] = useState(null)
+  const [startValidation,setStartValidation] = useState(null)
   
 
 
@@ -30,11 +31,18 @@ const SignupInput = forwardRef(({type,style,value,clearButtonMode,autoCapitalize
         setIsError(false)
       }
     }
+    if(type === 'password'){
+      if(value.trim().length < 10){
+        setIsError(true)
+      }else{
+        setIsError(false)
+      }
+    }
   },[value])
 
   return (
     <>
-    <TextInput style={[styles.input,style]} ref={ref ? ref : null} value={value} clearButtonMode={clearButtonMode ? clearButtonMode : 'never'} maxLength={maxLength ? maxLength : 30 } secureTextEntry={secureTextEntry ? secureTextEntry : false} clearTextOnFocus={clearTextOnFocus ? true : false} autoCapitalize={autoCapitalize} placeholder={placeholder} cursorColor={(isError && startValidation) ? 'red' : '#2D63E2'} keyboardType={keyboardType} onChange={onChange} onBlur={startValidationHandler}  />
+    <TextInput style={[styles.input,{borderColor:(isError && startValidation) ? 'red' : (isError === false && startValidation === true) ? '#2D63E2' : '#666'}]} ref={ref ? ref : null} value={value} clearButtonMode={clearButtonMode ? clearButtonMode : 'never'} maxLength={maxLength ? maxLength : 30 } secureTextEntry={secureTextEntry ? secureTextEntry : false} clearTextOnFocus={clearTextOnFocus ? true : false} autoCapitalize={autoCapitalize} placeholder={placeholder} cursorColor={(isError && startValidation) ? 'red' : '#2D63E2'} keyboardType={keyboardType} onChange={onChange} onBlur={startValidationHandler}  />
       {(isError && startValidation) && <Text style={[styles.errorText,{color:isError ? 'red' : '#2D63E2'}]}>{errorText}</Text>}
     </>
   )
