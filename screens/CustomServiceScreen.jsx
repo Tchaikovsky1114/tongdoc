d637097574371ad8ac7234835348d80ee151ea29
Tchaikovsky1114 <forwarm5891@gmail.com>
2022-11-16 13:07:43 +0900

    feat: 바텀 네비게이션 구현 및 라우트 정리

---
+++ screens/CustomServiceScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CustomServiceScreen() {
+  return (
+    <View>
+      <Text>CustomServiceScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
