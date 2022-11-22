1ee25e371005f2eff794e709993d23f4f2f0a183
Kim Myungseong <forwarm5891@gmail.com>
2022-11-22 15:24:46 +0900

    fix: icon,adaptive-icon 수정 및 빌드 최적화

--- app.config.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  return {
-    name: process.env.APP_ENV === 'production' ? 'tongdoc_app' : 'tongdoc_app(DEV)',
-    ios: {
-      bundleIdentifier: process.env.APP_ENV === 'production' ? 'com.dreamfreedom.tongdoc' : 'com.dreamfreedom.tongdoc-dev',
-    },
-    
-  };
-};
