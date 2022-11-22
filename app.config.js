51a04ff3d236430d0cb4bb70e70da6bd942defb5
Kim Myungseong <forwarm5891@gmail.com>
2022-11-22 13:55:28 +0900

    docs: app config 추가 및 app.json 수정

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default () => {
+  return {
+    name: process.env.APP_ENV === 'production' ? 'My App' : 'My App (DEV)',
+    ios: {
+      bundleIdentifier: process.env.APP_ENV === 'production' ? 'com.my.app' : 'com.my.app-dev',
+    },
+    
+  };
+};
